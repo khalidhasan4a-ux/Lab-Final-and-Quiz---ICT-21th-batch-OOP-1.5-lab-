87c9ebc237323f88a91a6d23aa4221abd3e80f9d
khalidhasan4a-ux <khalidhasan4a@gmail.com>
2026-01-28 15:27:23 +0600

    Update Encaptulation.java

--- Encaptulation.java
+++ Encaptulation.java
@@ -2,45 +2,38 @@ class Student {
     private int id;
     private String name;
     private double cgpa;
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getCgpa() {
         return cgpa;
     }
-
     public void setCgpa(double cgpa) {
         if (cgpa >= 0.0 && cgpa <= 4.0) {
             this.cgpa = cgpa;
         } else {
-            System.out.println("Invalid CGPA");
+            System.out.println("Invalid CGPA! Must be between 0.0 and 4.0");
         }
     }
 }
-
-public class  Encaptulation {
+public class Main {
     public static void main(String[] args) {
         Student s = new Student();
-        s.setId(1);
-        s.setName("Rakibul");
-        s.setCgpa(3.6);
-        System.out.println(s.getId());
-        System.out.println(s.getName());
-        System.out.println(s.getCgpa());
-        s.setCgpa(4.5);
+        s.setId(101);
+        s.setName("Md. Khalid Hasan");
+        s.setCgpa(3.76);
+        System.out.println("ID: " + s.getId());
+        System.out.println("Name: " + s.getName());
+        System.out.println("CGPA: " + s.getCgpa());
+        s.setCgpa(4.88);   
     }
 }
